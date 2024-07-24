9645d376dec801b4740a5038605f6c5c4cf6021b
Connor Nguyen <nguyendinhthanhcong2512@gmail.com>
2024-07-23 18:13:38 -0700

    Add 3 func to check condition, finishing the game

--- WordScramble/ContentView.swift
+++ WordScramble/ContentView.swift
@@ -12,14 +12,20 @@ struct ContentView: View {
     @State private var rootWord = ""
     @State private var newWord = ""
     
+    @State private var errorTitle = ""
+    @State private var errorMessage = ""
+    @State private var showingError = false
     
     var body: some View {
         NavigationStack{
             Text(rootWord)
                 .font(.largeTitle)
             List {
-                TextField("Enter your word", text: $newWord)
-                    .textInputAutocapitalization(.never)
+                Section{
+                    TextField("Enter your word", text: $newWord)
+                        .textInputAutocapitalization(.never)
+                        .disableAutocorrection(true)
+                }
             }
             Section {
                 ForEach(usedWords, id: \.self){ word in
@@ -33,6 +39,11 @@ struct ContentView: View {
         }
         .onSubmit(addNewWord)
         .onAppear(perform: startGame)
+        .alert(errorTitle, isPresented: $showingError){
+            Button("OK") {}
+        } message: {
+            Text(errorMessage)
+        }
         
     }
     func addNewWord() {
@@ -40,7 +51,21 @@ struct ContentView: View {
         
         guard newWord.count > 0 else { return }
         
-        //More code to come
+        guard isOriginal(word: answer) else {
+            wordError(title: "Not Original Word", message: "You must using existing English Word")
+            return
+        }
+        
+        guard isPossible(word: answer) else {
+            wordError(title: "Not possible", message: "You can't break the rule")
+            return
+        }
+        
+        guard isRealWord(word: answer) else {
+            wordError(title: "Not a real word", message: "Try using a real word, you can't just make word up")
+            return
+        }
+        
         withAnimation{
             usedWords.insert(answer, at: 0)
         }
@@ -58,6 +83,36 @@ struct ContentView: View {
         }
         
         fatalError("Could not load start.txt file")
+    }
+    
+    func isOriginal(word: String) -> Bool {
+        !usedWords.contains(word)
+    }
+    
+    func isPossible(word: String) -> Bool {
+        var tempWord = rootWord
+        for letter in word {
+            if let pos = tempWord.firstIndex(of: letter){
+                tempWord.remove(at: pos)
+            } else {
+                return false
+            }
+        }
+        
+        return true
+    }
+    
+    func isRealWord(word: String) -> Bool {
+        let checker = UITextChecker()
+        let range = NSRange(location: 0, length: word.utf16.count)
+        let missSpellRange = checker.rangeOfMisspelledWord(in: word, range: range, startingAt: 0, wrap: false, language: "en")
+        return missSpellRange.location == NSNotFound
+    }
+    
+    func wordError(title: String, message: String) {
+        errorTitle = title
+        errorMessage = message
+        showingError = true
     }
 }
 
